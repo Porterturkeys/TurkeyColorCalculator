200714391316c95147b0bf1a6378a13a95321862
Porterturkeys <163222788+Porterturkeys@users.noreply.github.com>
2026-01-17 20:24:01 -0500

    Create phenotypeMapping1E.js

---
+++ phenotypeMapping1E.js
@@ -0,0 +1,50 @@
+const phenotypeMapping1E = {
+
+   // bb ee CHOCOLATE NEBRASKAN
+"bb ee spsp": "Chocolate Nebraskan",
+"bb ee Nn spsp": "Chocolate Nebraskan",
+"bb ee Slsl spsp": "Chocolate Nebraskan",
+"bb ee Pnpn spsp": "Chocolate Nebraskan",
+"bb ee pnpn spsp": "Chocolate Nebraskan",
+"bb ee Nn Slsl spsp": "Chocolate Nebraskan",
+"bb ee Nn Pnpn spsp": "Chocolate Nebraskan",
+"bb ee Nn pnpn spsp": "Chocolate Nebraskan",
+"bb ee Nn Pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee Nn pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee Pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee pnpn Slsl spsp": "Chocolate Nebraskan",
+
+// bb ee fdfd CHOCOLATE NEBRASKAN
+"bb ee fdfd spsp": "Chocolate Nebraskan",
+"bb ee fdfd Nn spsp": "Chocolate Nebraskan",
+"bb ee fdfd Slsl spsp": "Chocolate Nebraskan",
+"bb ee fdfd Pnpn spsp": "Chocolate Nebraskan",
+"bb ee fdfd pnpn spsp": "Chocolate Nebraskan",
+"bb ee fdfd Nn Pnpn spsp": "Chocolate Nebraskan",
+"bb ee fdfd Nn pnpn spsp": "Chocolate Nebraskan",
+"bb ee fdfd Nn Slsl spsp": "Chocolate Nebraskan",
+"bb ee fdfd Nn Pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee fdfd Nn pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee fdfd Pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee fdfd pnpn Slsl spsp": "Chocolate Nebraskan",
+
+// bb ee Fdfd CHOCOLATE NEBRASKAN
+"bb ee Fdfd spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Nn spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Slsl spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Pnpn spsp": "Chocolate Nebraskan",
+"bb ee Fdfd pnpn spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Nn Pnpn spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Nn pnpn spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Nn Slsl spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Nn Pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Nn pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee Fdfd Pnpn Slsl spsp": "Chocolate Nebraskan",
+"bb ee Fdfd pnpn Slsl spsp": "Chocolate Nebraskan",
+ 
+    
+  
+
+
+
+};
