2a5c21a65060d4104b8e61f72af5a1b8cf01f111
Porterturkeys <163222788+Porterturkeys@users.noreply.github.com>
2026-01-19 02:24:58 -0500

    Update Various-Functions.js

--- Various-Functions.js
+++ Various-Functions.js
@@ -10,32 +10,29 @@ const scripts = [
 "https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/SplitFunction.js",  
 "https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/TransfertoParent.js",  
    
-   
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping1.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping1A.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping1B.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping1C.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping1D.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping1E.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping2.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping2A.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping3.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping3A.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping4.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping5.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping6.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping7.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping8.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping9.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping10.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping11.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping12.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping13.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping14.js",
+"https://raw.githubusercontent.com/Porterturkeys/TurkeyColorCalculator/refs/heads/main/phenotypeMapping15.js"
 
-  
-  
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1A.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1B.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1C.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1D.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1E.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping2.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping2A.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping3.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping3A.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping4.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping5.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping6.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping7.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping8.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping9.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping10.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping11.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping12.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping13.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping14.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping15.js"
 
 ];
 
