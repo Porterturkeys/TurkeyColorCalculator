d2c99a798d6ba86a886766c87bd38d3425b0a5c6
Porterturkeys <163222788+Porterturkeys@users.noreply.github.com>
2026-02-13 11:52:33 -0500

    Update share URL to TurkeyColorCalculator.com

--- SocialShare.js
+++ SocialShare.js
@@ -16,7 +16,7 @@
         shareSection.style.textAlign = "center";
         shareSection.style.marginTop = "40px"; // optional spacing from content
 
-        var url = "https://porters-rare-heritage-turkeys.neocities.org/TurkeyColorCalculators";
+        var url = "https://TurkeyColorCalculator.com";
 
         shareSection.innerHTML = `
             <p style="font-weight:bold;margin-bottom:10px;">Share This Calculator:</p>
