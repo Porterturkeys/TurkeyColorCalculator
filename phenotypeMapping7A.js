969f509ea1bed53ce83630cabf0075fc0ae7564e
Porterturkeys <163222788+Porterturkeys@users.noreply.github.com>
2026-02-20 17:36:22 -0500

    Update phenotypeMapping7A.js

--- phenotypeMapping7A.js
+++ phenotypeMapping7A.js
@@ -1,4 +1,203 @@
 const phenotypeMapping7A = {
+// PENCILLED BLUE WINGED RED SLATE
+
+// Base
+"b1b1 Dd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+// sp
+"b1b1 Dd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+
+// Fd
+"b1b1 Dd Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn Fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn Fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+"b1b1 Dd fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+// Fd + sp
+"b1b1 Dd Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn Fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn Fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+
+"b1b1 Dd fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Ee Nn fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Dd Nn fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+
+// Ccm group
+"b1b1 Ccm Dd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+// sp
+"b1b1 Ccm Dd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+
+// Fd
+"b1b1 Ccm Dd Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn Fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn Fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+"b1b1 Ccm Dd fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Ee Nn fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Ccm Dd Nn fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+// Cc group
+
+// Base
+"b1b1 Cc Dd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+// sp
+"b1b1 Cc Dd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+
+// Fd
+"b1b1 Cc Dd Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn Fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn Fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn Fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+"b1b1 Cc Dd fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn fdfd pnpn": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn fdfd pnpn Slsl": "Pencilled Blue Winged Red Slate",
+
+// Fd + sp
+"b1b1 Cc Dd Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn Fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn Fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn Fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+
+"b1b1 Cc Dd fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Ee Nn fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn fdfd pnpn Spsp": "Pencilled Blue Winged Red Slate",
+"b1b1 Cc Dd Nn fdfd pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate",
+
+
+// PENCILLED BLUE WINGED RED SLATE, Pnpn
+
+// Base
+"b1b1 Dd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+// sp
+"b1b1 Dd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+// Fd
+"b1b1 Dd Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Fdfd Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Fdfd Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+"b1b1 Dd fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn fdfd Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn fdfd Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+// Fd + sp
+"b1b1 Dd Fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn Fdfd Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn Fdfd Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+"b1b1 Dd fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Ee Nn fdfd Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn fdfd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Dd Nn fdfd Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+// Ccm group
+"b1b1 Ccm Dd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Nn Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Nn Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Nn Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Nn Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+// sp
+"b1b1 Ccm Dd Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Nn Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Nn Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Nn Pnpn Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Nn Pnpn Slsl Spsp": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+
+// Fd
+"b1b1 Ccm Dd Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Nn Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Ee Nn Fdfd Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Nn Fdfd Pnpn": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
+"b1b1 Ccm Dd Nn Fdfd Pnpn Slsl": "Pencilled Blue Winged Red Slate (Semi-Pencilled)",
 
 
 
